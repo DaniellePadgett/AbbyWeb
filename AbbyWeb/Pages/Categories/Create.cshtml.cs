76d3e925dc369e8b701ab1de668faa0674bd2812
danielle.padgett <Danielle.Padgett@sharp.eu>
2022-06-21 14:02:11 +0100

    1

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,13 +1,31 @@
+using AbbyWeb.Data;
 using AbbyWeb.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AbbyWeb.Pages.Categories
 {
 	public class CreateModel : PageModel
 	{
+		private readonly ApplicationDbContext _db;
+		[BindProperty]
 		public Category Category { get; set; }
+
+		public CreateModel(ApplicationDbContext db)
+		{
+			_db = db;
+
+		}
+
 		public void OnGet()
 		{
+		}
+
+		public async Task<IActionResult> OnPost(Category category)
+		{
+			await _db.Category.AddAsync(category);
+			await _db.SaveChangesAsync();
+			return RedirectToPage("Index");
 		}
 	}
 }
